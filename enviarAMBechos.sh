b73295ed5e867b40fd984d5b3f8bbade3a8f20a8
kerojohan <keroseno@gmail.com>
2014-02-09 08:55:15 +0100

    servidor net

--- enviarAMBechos.sh
+++
@@ -1,6 +0,0 @@
-stty -F /dev/ttyUSB0 9600;
-echo "f" > /dev/ttyUSB0;
-sleep 5;
-#echo "c" > /dev/ttyUSB0;
-#echo `head -n1 /dev/ttyUSB0& echo -e "c\\r"> /dev/ttyUSB0`;
-echo `head -n1 /dev/ttyUSB0& echo -e "c\\r"> /dev/ttyUSB0`;
